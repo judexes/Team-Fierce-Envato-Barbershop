96d130d3c7e325bb679bfac96d3e86682a00aff2
jude okoroafor <judeokoroafor@gmail.com>
2020-06-20 03:27:51 +0100

    Create header-footer.html and update

---
+++ includes/footer.php
@@ -0,0 +1,68 @@
+<footer class="">
+    <div class="container-fluid footer-main">
+        <div class="row justify-content-md-center">
+            <div class="col-md-3">
+                <h5>FIERCE CUTS</h5>
+                <ul class="list-unstyled text-small">
+                    <br>
+                    <li><h6>ADDRESS</h6></li>
+                    <li class="text-muted">13B Reid road, Lagos Cresent,<br> Lagos.</li>
+                    <br>
+                    <li><h6>HOURS:</h6></li>
+                    <li class="text-muted">MONDAY - FRIDAY: 9 AM - 5 PM</li>
+                    <li class="text-muted">SATURDAY & SATURDAY: 8 AM - 10 PM</li>
+                    <br>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="list-unstyled text-small quick-links">
+                    <li><a class="" href="#">ABOUT US</a></li>
+                    <li><a class="" href="#">CONTACT US</a></li>
+                    <li><a class="" href="#">PRICING</a></li>
+                    <li><a class="" href="#">GALLERY</a></li>
+                    <li><a class="" href="#">CAREERS</a></li>
+                    <br>
+                </ul>
+            </div>
+            <div class="col-md-4 subscribe">
+                <h6>SUBSCRIBE TO OUR NEWSLETTER</h6>
+                <ul class="list-unstyled text-small">
+                    <li><a class="" href="#"></a></li>
+                    <li>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Enter Your Email Address" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button">SUBSCRIBE</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid footer-copy">
+        <div class="row">
+            <div class="col-7 offset-2"><span class="text-muted">Copyright 2020- All Rights Reserved</span></div>
+            <div class="col-1">
+                <ul class="list-group list-group-horizontal">
+                    <li class="nav-item">
+                        <a class="" href="#">
+                            <img class="phone img-fluid" src="assets/images/facebook.png" alt="">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#">
+                            <img class="phone img-fluid" src="assets/images/twitter.png" alt="">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#">
+                            <img class="phone img-fluid" src="assets/images/instagram.png" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</footer>
